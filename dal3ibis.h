f99421a2714714329bfd3be469de4bba503b1ae2
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-04-13 11:20:28 +0200

    template alternative

--- dal3ibis.h
+++ dal3ibis.h
@@ -135,20 +135,11 @@
 /*****************************************************************************/
 
 #include <dal3gen.h>
+#include <dal3ibis_version.h>
 
 #ifndef DAL3IBIS_INCLUDE
 #define DAL3IBIS_INCLUDE
 
-{% if mode == "delivery" %}
-static const char * dal3ibisversion[] = {"ISDC component {{component_name}} {{component_version}}"};
-{% else %}
-#include "git_version.h"
-
-#ifndef GIT_VERSION
-#define GIT_VERSION ""
-#endif
-static const char * dal3ibisversion[] = {"ISDC component {{component_name}} {{component_version}}"};
-{% endif %}
 
 
 	/* DAL3IBIS ERROR                                                    */
