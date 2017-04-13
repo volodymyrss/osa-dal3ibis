2f6ccf8b3994d45eb890e508baff31abeded5bd7
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-04-13 13:29:16 +0200

    complilabel clone

--- dal3ibis.h
+++ dal3ibis.h
@@ -138,17 +138,12 @@
 
 #ifndef DAL3IBIS_INCLUDE
 #define DAL3IBIS_INCLUDE
-
-{% if mode == "delivery" %}
-static const char * dal3ibisversion[] = {"ISDC component {{component_name}} {{component_version}}"};
-{% else %}
 #include "git_version.h"
 
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
-static const char * dal3ibisversion[] = {"ISDC component {{component_name}} {{component_version}}"};
-{% endif %}
+static const char * dal3ibisversion[] = {"ISDC component dal3ibis DEBUG"};
 
 	/* DAL3IBIS ERROR                                                    */
 
